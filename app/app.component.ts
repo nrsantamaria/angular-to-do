b23f840b5ed6498a7ffd87ada7016d8f3cc60608
Ben Williams and Nicole Santamaria <nicole@>
2017-06-06 13:37:03 -0700

    add ngIf statement to hide form before clicking the edit button.

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,7 +11,7 @@ import { Component } from '@angular/core';
       <ul>
         <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
       </ul>
-      <div>
+      <div *ngIf="selectedTask">
         <h3>{{selectedTask.description}}</h3>
         <p>Task Complete? {{selectedTask.done}}</p>
         <h3>Edit Task</h3>
@@ -22,6 +22,7 @@ import { Component } from '@angular/core';
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
       </div>
     </div>
   `
@@ -39,7 +40,11 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
+  
+  finishedEditing(){
+    this.selectedTask = null;
+  }
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
